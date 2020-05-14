626e0a7f47c5f208b15645d80b547e993ec08149
liushanshan3323 <501202245@qq.com>
2020-05-14 23:42:49 +0800

    第九次提交

--- js/1.js
+++ js/1.js
@@ -7,8 +7,6 @@ let newProducts = (function newProducts() {
         slick_prev = bd.querySelector('.slick-prev'),
         slick_next = bd.querySelector('.slick-next');
     let step = 0,
-        interval = 1000,
-        autoTimer = null,
         len = item.length;
 
     function autoMove() {
@@ -59,70 +57,188 @@ let indexPopular = (function indexPopular() {
             change(this.zhufeng);
         };
     }
+})();
+// 限时购倒计时
+let countDown = (function countDown() {
+    let box = document.querySelector('.flashSaleLt'),
+        content = document.querySelector('.m-countDown'),
+        hour = content.querySelector('.j-hour'),
+        minute = content.querySelector('.j-minute'),
+        second = content.querySelector('.j-second'),
+        timer = null;
+    console.log(hour);
 
+    // 获取服务器时间
+    function getServerTime() {
+        return new Promise(resolve => {
+            let xhr = new XMLHttpRequest;
+            xhr.open('head', './data.json?_=' + Math.random());
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 2 && /^(2|3)\d{2}$/.test(xhr.status)) {
+                    let time = xhr.getResponseHeader('date');
+                    time = new Date(time);
+                    resolve(time);
+                }
+            };
+            xhr.send();
+        });
+    }
+
+    // 根据服务器时间计算倒计时
+    function computed(time) {
+        // time从服务器获取的当时间
+        // target是抢购的目标时间
+        // spanTime两个时间的毫秒差
+        let target = new Date('2020/05/18 10:00:00'),
+            spanTime = target - time;
+        if (spanTime <= 0) {
+            // 已经到达抢购的时间节点了
+            clearInterval(timer);
+            return;
+        }
+        // 计算出毫秒差中包含多少小时、多少分钟、多少秒
+        let hours = Math.floor(spanTime / (60 * 60 * 1000));
+        spanTime = spanTime - hours * 60 * 60 * 1000;
+        let minutes = Math.floor(spanTime / (60 * 1000));
+        spanTime = spanTime - minutes * 60 * 1000;
+        let seconds = Math.floor(spanTime / 1000);
+        hours < 10 ? hours = '0' + hours : null;
+        minutes < 10 ? minutes = '0' + minutes : null;
+        seconds < 10 ? seconds = '0' + seconds : null;
+        hour.innerHTML = `${hours}`;
+        minute.innerHTML = `${minutes}`;
+        second.innerHTML = `${seconds}`;
+    }
+
+    getServerTime().then(time => {
+        // 获取到服务器时间后，计算倒计时
+        computed(time);
+
+        // 每间隔1秒中，让获取的时间累加1秒，在重新计算倒计时结果
+        timer = setInterval(() => {
+            time = new Date(time.getTime() + 1000);
+            computed(time);
+        }, 1000);
+    });
+})();
+// 居家生活及以下广告
+function move(ele) {
+    let banner = document.querySelector(ele),
+        slidesList = banner.querySelectorAll('.slides'),
+        slickoDotsList = banner.querySelectorAll('.slick-dots>li'),
+        slickPrev = banner.querySelector('.slick-prev'),
+        slickNext = banner.querySelector('.slick-next');
+    let step = 0,
+        prev = 0,
+        len = slidesList.length;
+    console.log(len)
+
+    //实现切换
+    function change() {
+        let cur = slidesList[step],
+            pre = slidesList[prev];
+        cur.style.zIndex = '1';
+        pre.style.zIndex = '0';
+        cur.style.transition = '.3s';
+        cur.style.opacity = '1';
+        pre.style.transition = '0.3s';
+        pre.style.opacity = '0';
+        slickoDots();
+    }
+    //AUTOMOVE自动切换
+    function autoMove() {
+        prev = step;
+        step++;
+        step > (len - 1) ? step = 0 : null;
+        change();
+    }
+    //焦点对齐
+    function slickoDots() {
+        let tempStep = step;
+        if (tempStep === len) {
+            tempStep = 0;
+        }
+        slickoDotsList.forEach((item, index) => {
+            if (index === tempStep) {
+                item.className = 'slick-active';
+                return;
+            } else {
+                item.className = '';
+            }
+
+        })
+    }
+    //点击移动切换
+    Array.from(slickoDotsList).forEach((item, index) => {
+        item.onmousemove = function () {
+            if (index === step) {
+                return
+            }
+            prev = step;
+            step = index;
+            change();
+        }
+    })
+
+    //点击右边按钮实现切换
+    slickNext.onclick = function () {
+        autoMove();
+    }
+    //点击左边按钮实现切换
+    slickPrev.onclick = function () {
+        prev = step;
+        step--;
+        step < 0 ? step = len - 1 : null;
+        change();
+    }
+}
+move('.banner1');
+move('.banner2');
+move('.banner3');
+move('.banner4');
+move('.banner5');
+move('.banner6');
+move('.banner7');
+move('.banner8');
+let Products = (function Products() {
+    bd = document.querySelector('.slickWidth'),
+        itemList = bd.querySelector('.itemList'),
+        total_track = itemList.querySelector('.total-track'),
+        item = total_track.querySelectorAll('.item'),
+        slick_prev = bd.querySelector('.slick-prev'),
+        slick_next = bd.querySelector('.slick-next');
+    let step = 0,
+        len = item.length;
+
+    function autoMove() {
+        if (step === (len-3)) {
+            step = 0;
+            total_track.style.transitionDuration = '3s';
+            total_track.style.left = '0px';
+            total_track.offsetWidth;
+        }
+        step++;
+        total_track.style.transitionDuration = '0.3s';
+        total_track.style.left = -step * 365 + 'px';
+        console.log(step);
+
+    }
+    slick_next.onclick = autoMove;
+    slick_prev.onclick = function () {
+        if (step === 0) {
+            step = 18;
+            total_track.style.transitionDuration = '0s';
+            total_track.style.left = -step * 365 + 'px';
+            total_track.offsetWidth;
+        }
+        step--;
+        total_track.style.transitionDuration = '0.3s';
+        total_track.style.left = -step * 365+ 'px';
+    };
     return {
-        init() {}
+        init() {
+            autoMove();
+        }
     }
 })();
-indexPopular.init();
-let box = document.querySelector('.flashSaleLt'),
-    content = document.querySelector('.m-countDown'),
-    hour = content.querySelector('.j-hour'),
-    minute = content.querySelector('.j-minute'),
-    second = content.querySelector('.j-second'),
-    timer = null;
-console.log(hour);
-
-// 获取服务器时间
-function getServerTime() {
-    return new Promise(resolve => {
-        let xhr = new XMLHttpRequest;
-        xhr.open('head', './data.json?_=' + Math.random());
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 2 && /^(2|3)\d{2}$/.test(xhr.status)) {
-                let time = xhr.getResponseHeader('date');
-                time = new Date(time);
-                resolve(time);
-            }
-        };
-        xhr.send();
-    });
-}
-
-// 根据服务器时间计算倒计时
-function computed(time) {
-    // time从服务器获取的当时间
-    // target是抢购的目标时间
-    // spanTime两个时间的毫秒差
-    let target = new Date('2020/05/15 15:57:35'),
-        spanTime = target - time;
-    if (spanTime <= 0) {
-        // 已经到达抢购的时间节点了
-        box.innerHTML = "开始抢购！";
-        clearInterval(timer);
-        return;
-    }
-    // 计算出毫秒差中包含多少小时、多少分钟、多少秒
-    let hours = Math.floor(spanTime / (60 * 60 * 1000));
-    spanTime = spanTime - hours * 60 * 60 * 1000;
-    let minutes = Math.floor(spanTime / (60 * 1000));
-    spanTime = spanTime - minutes * 60 * 1000;
-    let seconds = Math.floor(spanTime / 1000);
-    hours < 10 ? hours = '0' + hours : null;
-    minutes < 10 ? minutes = '0' + minutes : null;
-    seconds < 10 ? seconds = '0' + seconds : null;
-    hour.innerHTML = `${hours}`;
-    minute.innerHTML = `${minutes}`;
-    second.innerHTML = `${seconds}`;
-}
-
-getServerTime().then(time => {
-    // 获取到服务器时间后，计算倒计时
-    computed(time);
-
-    // 每间隔1秒中，让获取的时间累加1秒，在重新计算倒计时结果
-    timer = setInterval(() => {
-        time = new Date(time.getTime() + 1000);
-        computed(time);
-    }, 1000);
-});
+Products.init();
--- js/wangyi.js
+++ js/wangyi.js
@@ -76,3 +76,81 @@ $(function () {
 		change();
 	});
 });
+
+/*
+ * DOM结构加载完触发执行（可以获取到页面中的DOM元素）
+ * 形成一个闭包 
+//  */
+// $(function () {
+// 	let $banner = $('.banner'),
+// 		$slidesList = $banner.find('.slides'),
+// 		$slickoDotsList = $banner.find('.slick-dots>li'),
+// 		$slickPrev  = $banner.children('.slick-prev '),
+// 		$slickNext = $banner.children('.slick-next');
+// 	let step = 0,
+// 		prev = 0,
+// 		len = $slidesList.length;
+
+// 	// CHANGE实现切换
+// 	function change() {
+// 		let $cur = $slidesList.eq(step),
+// 			$pre = $slidesList.eq(prev);
+// 		$cur.css('zIndex', 1);
+// 		$pre.css('zIndex', 0);
+
+// 		$cur.css({
+// 			transitionDuration: '.3s',
+// 			opacity: 1
+// 		}).one('transitionend', () => {
+// 			$pre.css({
+// 				transitionDuration: '0s',
+// 				opacity: 0
+// 			});
+// 		});
+
+// 		// 焦点自动对齐
+// 		// $paginationList.eq(step).addClass('active')
+// 		// 	.siblings().removeClass('active');
+// 		$slickoDotsList.each((index, item) => {
+// 			if (index === step) {
+// 				$(item).addClass('slick-active');
+// 				return;
+// 			}
+// 			$(item).removeClass('slick-active');
+// 		});
+// 	}
+
+// 	// AUTOMOVE自动切换
+// 	function autoMove() {
+// 		prev = step;
+// 		step++;
+// 		step > (len - 1) ? step = 0 : null;
+// 		change();
+// 	}
+	
+// 	// // 加载页面开始自动切换
+// 	// autoTimer = setInterval(autoMove, interval);
+
+// 	// // 鼠标进入和离开控制自动的关闭和开启
+// 	// $slickoDotsList.on('mouseenter', () => clearInterval(autoTimer))
+// 	// 	.on('mouseleave', () => autoTimer = setInterval(autoMove, interval));
+
+// 	// 点击焦点切换
+// 	$slickoDotsList.on('click', function () {
+// 		//  $(this).index() 当前点击这一项的索引
+// 		let index = $(this).index();
+// 		if (index === step) return;
+// 		prev = step;
+// 		step = index;
+// 		change();
+// 	});
+
+// 	// 点击左右按钮实现切换
+// 	$slickNext.on('click', autoMove);
+// 	$slickPrev.on('click', function () {
+// 		prev = step;
+// 		step--;
+// 		step < 0 ? step = len - 1 : null;
+// 		change();
+// 	});
+// });
